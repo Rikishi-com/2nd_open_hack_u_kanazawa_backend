1984612488b8ba93821f073e10c33ea44033d33f
Yuki Ueno <145101888+Rikishi-com@users.noreply.github.com>
2025-09-17 15:28:33 +0900

    Revert "add from shimaken"
    This reverts commit ffc59464d3c6aba09a92d6c88db411307ec99e81.

--- myproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from generator import views  # 👈 追加
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('generator/', include('generator.urls')),
-    path('', views.index, name="home"),  # 👈 ルートURLを直でビューに割り当て
-]
